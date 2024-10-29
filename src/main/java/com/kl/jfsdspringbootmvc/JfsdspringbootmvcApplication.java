16405b627a9ea90f0443e73381f441f2bbf99617
lohit <lohithr60@gmail.com>
2024-10-29 15:47:50 +0530

    first commit

---
+++ src/main/java/com/kl/jfsdspringbootmvc/JfsdspringbootmvcApplication.java
@@ -0,0 +1,14 @@
+package com.kl.jfsdspringbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdspringbootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdspringbootmvcApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/kl/jfsdspringbootmvc/JfsdspringbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.jfsdspringbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfsdspringbootmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
